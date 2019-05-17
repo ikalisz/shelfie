6a53ba71cc21ea93704b6e3e723b432ea471ee4b
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-17 11:41:17 -0600

    Set up most of form functionality

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './Form.css'
+import axios from 'axios'
+
+export default class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            price: 0,
+            img: '',
+            changed: false,
+        }
+        this.handleResetState = this.handleResetState.bind(this)
+    }
+
+    handleResetState() {
+        this.setState({
+            name: '',
+            price: 0,
+            img: '',
+            changed: false,
+        })
+    }
+
+    componentDidUpdate = () => {
+       this.setState({changed: true})
+    }
+
+    handleUpdateImg = (e) => {
+        this.setState({img: e.target.value})
+    }
+
+    handleUpdateName = (e) => {
+        this.setState({name: e.target.value})
+    }
+
+    handleUpdatePrice = (e) => {
+        this.setState({price: e.target.value})
+    }
+
+    handleAddToInventory = () => {
+        const {name, price, img} = this.state
+    }
+
+    render() {
+        return (
+            <div className="formAddDiv">
+                <img />
+                <div className="imageUrlDiv">
+                    <p>Image URL:</p>
+                    <input type="text" placeholder="Product Image Url here!" onChange={(e) => this.handleUpdateImg(e)} />
+                </div>
+                <div className="productNameDiv">
+                    <p>Product Name:</p>
+                    <input type="text" placeholder="Product name here!" />
+                </div>
+                <div className="productPriceDiv">
+                    <p>Price:</p>
+                </div>
+                <div className="formButtonsDiv">
+                    <Link to='/'><button onClick={this.handleResetState}>Cancel</button></Link>
+                    {!this.state.changed?
+                    <div>Type Something in the forms!</div>
+                    :
+                    <Link to='/'><button>Add to Inventory</button></Link>
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
